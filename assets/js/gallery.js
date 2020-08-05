14e040e535a1fd0c28170c6b5e7ae1039d2e2a56
Mauricio Meza <mmezab@unal.edu.co>
2020-08-04 23:51:30 -0500

    Added carousel funtionality to modal imgs

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -19,6 +19,7 @@ function loadGallery(condition){
 
         $(data).each((i, trabajo) => {
             var type;
+            
             if(condition == 'all'){
                 type = trabajo.tipo
             }else{
@@ -43,15 +44,39 @@ function loadGallery(condition){
                     + '</div>' 
                 + '</div>')
                 gal.append(column)
-
-
+                
+                var carousel = ''
+                carousel += trabajo.secondaryimgs.map((img, j) => {
+                            if(j == 0){
+                                return '<div class="carousel-item active">' +
+                                        '<img class="img-fluid d-block mx-auto" src="assets/img/port/' + img + '" alt="img' + i + '">' +
+                                    '</div>'
+                            }else{
+                                return '<div class="carousel-item">' +
+                                        '<img class="img-fluid d-block mx-auto" src="assets/img/port/' + img + '" alt="img' + i + '">' +
+                                    '</div>'   
+                            }
+                               
+                        }).join(' ')
+                
                 var modal = $(
                     '<div class="modal fade portfolio-modal text-center" role="dialog" tabindex="-1" id="portfolioModal'+ i +'">'
                         + '<div class="modal-dialog modal-lg" role="document">'
                             + '<div class="modal-content content">'
                                 + '<button class="btn btn-primary boton-cerrar" data-dismiss="modal" type="button"><i class="fa fa-times"></i><span>&nbsp;Cerrar</span></button>'
                                 + '<div class="modal-body">'
-                                    + '<img class="img-fluid d-block mx-auto" src="assets/img/port/'+ trabajo.secondaryimgs[0] +'">'
+                                    + '<div id="imgcarousel'+ i +'" class="carousel slide" data-ride="carousel">' +
+                                    '<div class="carousel-inner">' +
+                                    carousel +
+                                    '</div>' +
+                                    '<a class="carousel-control-prev" href="#imgcarousel' + i + '" role="button" data-slide="prev">' +
+                                        '<span class="carousel-control-prev-icon" aria-hidden="true"></span>' +
+                                        '<span class="sr-only">Anterior</span>' +
+                                    '</a>' +
+                                    '<a class="carousel-control-next" href="#imgcarousel' + i + '" role="button" data-slide="next">' +
+                                        '<span class="carousel-control-next-icon" aria-hidden="true"></span>' +
+                                        '<span class="sr-only">Siguiente</span>' +
+                                    '</a>'
                                     + '<p class="project-info"> <b> Nombre: </b> '+ trabajo.nombre +' </p>'
                                     + '<p class="project-info"> <b> Tipo: </b> '+ trabajo.tipo +' </p>'
                                     + '<p class="project-info"> <b> Año: </b> '+ trabajo.fecha +' </p>'
