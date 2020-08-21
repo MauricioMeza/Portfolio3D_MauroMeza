ae434d15408e5281e7962f5bfde25eac386f5834
Mauricio Meza <mmezab@unal.edu.co>
2020-08-21 01:35:02 -0500

    Version with hopefully less loadtime

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -17,7 +17,7 @@ function loadGallery(condition){
         var gal = $("#portfolio-gallery")
         var mod = $("#modals")
         gal.empty()
-        mod.empty()
+        //mod.empty()
 
         $(data).each((i, trabajo) => {
             var type;
@@ -32,7 +32,7 @@ function loadGallery(condition){
                 
                 var column = $(
                 '<div class="col-sm-6 col-md-4 portfolio-item">' 
-                    + '<a class="portfolio-link" data-toggle="modal" href="#portfolioModal'+ i +'">' 
+                    + '<a class="portfolio-link" data-toggle="modal" href="#portfolioModalmain" onclick="openModal(' + i +')">' 
                         + '<div class="portfolio-hover port-hover">' 
                             + '<div class="portfolio-hover-content">'
                                 + '<i class="fa fa-plus fa-3x"></i>'
@@ -47,33 +47,8 @@ function loadGallery(condition){
                 + '</div>')
                 gal.append(column)
                 
-                var carousel = ''
-                if(trabajo.tipo == "Animacion"){
-                    carousel += '<div class="carousel-item active">' +
-                                        '<iframe width="640" height="385" src="' + trabajo.video + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' +
-                                    '</div>'        
-                }else{
-                    carousel += trabajo.secondaryimgs.map((img, j) => {
-                        if(j == 0){
-                            return '<div class="carousel-item active">' +
-                                    '<img class="img-fluid d-block mx-auto" src="assets/img/port/' + img + '" alt="img' + i + '">' +
-                                '</div>'
-                        }else{
-                            return '<div class="carousel-item">' +
-                                    '<img class="img-fluid d-block mx-auto" src="assets/img/port/' + img + '" alt="img' + i + '">' +
-                                '</div>'                                   
-                        }
-                           
-                    }).join(' ')
-            
-                    if(trabajo.prev3d){
-                        carousel += '<div class="carousel-item">' +
-                                        '<iframe allowfullscreen webkitallowfullscreen width="100%" height="400" frameborder="0" seamless src="' + trabajo.prev3d +'"></iframe>' +
-                                    '</div>'
-                    }    
-                }
 
-                
+                /*
                 var modal = $(
                     '<div class="modal fade portfolio-modal text-center" role="dialog" tabindex="-1" id="portfolioModal'+ i +'">'
                         + '<div class="modal-dialog modal-lg" role="document">'
@@ -103,8 +78,54 @@ function loadGallery(condition){
                     + '</div>')
 
                 mod.append(modal)
+                */
             }
         })
-
     })
+}
+
+function openModal(i){
+    var carin = $("#carousel-innermain")
+    carin.empty()
+    $("#main-name").empty()
+    $("#main-type").empty()
+    $("#main-year").empty()
+    $("#main-info").empty()
+
+    $.getJSON('./portfolio.json', data =>{
+        trabajo = data[i];
+
+        var carousel = ''
+        if(trabajo.tipo == "Animacion"){
+            carousel += '<div class="carousel-item active">' +
+                                '<iframe width="640" height="385" src="' + trabajo.video + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' +
+                            '</div>'        
+        }else{
+            carousel += trabajo.secondaryimgs.map((img, j) => {
+                if(j == 0){
+                    return '<div class="carousel-item active">' +
+                            '<img class="img-fluid d-block mx-auto" src="assets/img/port/' + img + '" alt="img' + i + '">' +
+                        '</div>'
+                }else{
+                    return '<div class="carousel-item">' +
+                            '<img class="img-fluid d-block mx-auto" src="assets/img/port/' + img + '" alt="img' + i + '">' +
+                        '</div>'                                   
+                }
+                    
+            }).join(' ')
+    
+            if(trabajo.prev3d){
+                carousel += '<div class="carousel-item">' +
+                                '<iframe allowfullscreen webkitallowfullscreen width="100%" height="400" frameborder="0" seamless src="' + trabajo.prev3d +'"></iframe>' +
+                            '</div>'
+            }    
+        }
+        carin.append(carousel)
+
+        $("#main-name").append('<b> Nombre: </b>'+ trabajo.nombre)
+        $("#main-type").append('<b> Tipo: </b>'  + trabajo.tipo)
+        $("#main-year").append('<b> Fecha: </b>' + trabajo.fecha)
+        $("#main-info").append('<b> Info: </b>'  + trabajo.info)
+
+    })       
 }
